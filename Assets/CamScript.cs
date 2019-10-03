ef7b007e537c15484bf3c718ebbf29b116b7af18
truongph <truongph@buffalo.edu>
2019-10-02 22:18:24 -0400

    init
    initial commit

---
+++ Assets/CamScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+    Rigidbody camera0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera0 = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            camera0.AddRelativeForce(Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            camera0.AddRelativeForce(-Vector3.up);
+        }
+    }
+}
---
+++ Assets/Rocket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Thrust();
+        Rotate();
+	}
+
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) // so it doesn't layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void Rotate()
+    {
+        rigidBody.freezeRotation = false;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+
+        rigidBody.freezeRotation = true;
+    }
+
+}
